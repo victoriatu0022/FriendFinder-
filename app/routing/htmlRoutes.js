31932f99e6d2e6dd7766dd4505afd4b6450898b8
Victoria Tu <victoriatu0022@gmail.com>
2018-03-13 16:16:57 -0500

    html pages and character update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,7 +6,6 @@ module.exports = function(app){
 	app.get("/home", function(req, res) {
     	res.sendFile(path.join(__dirname, "../public/home.html"));
   	});
-	//path to the survey
 	app.get("/friendsSurvey", function(req, res) {
     	res.sendFile(path.join(__dirname, "../public/survey.html"));
   	});
